496835b188b8e5e33c5a753d1162e2a197e8b2a9
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2019-10-17 08:27:56 +0200

    Added auto updating functionality

--- functions.php
+++ functions.php
@@ -5,6 +5,12 @@ include_once get_stylesheet_directory() . '/vendor/autoload.php';
 
 add_theme_support('custom-logo');
 
+$updates = \Puc_v4_Factory::buildUpdateChecker(
+'https://github.com/DoedeJaarsmaCommunicatie/Sanering',
+__FILE__,
+'sanering',
+'2'
+);
 
 Timber::$locations = [
     get_stylesheet_directory() . '/templates/',
